5d8257d30dcc7de13d62ac73c575ac0696e07f1c
mpro7 <justinstarlight@gmail.com>
2019-03-15 23:30:18 +0100

    added list and user components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { SingleUserComponent } from './user-list/single-user/single-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    SingleUserComponent
   ],
   imports: [
     BrowserModule
